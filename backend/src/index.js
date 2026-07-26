9046dc37834622fcc1c77c158ff850f07cc246f2
Alicia Fernández <110678047+BlueAletheia@users.noreply.github.com>
2026-07-26 19:05:30 +0200

    intentando que se despliegue en el servidor
    const PORT = process.env.PORT || 3000

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import reservasRoutes from './routes/reservas.routes.js'
 import contactoRoutes from './routes/contacto.routes.js'
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
